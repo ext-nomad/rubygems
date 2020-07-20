7d03f678a694db776b7cf89ed33e1eb0a3a266de
ext-nomad <i.dwanderer@gmail.com>
2020-07-20 15:39:01 +0300

    gem friendly_id

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,5 +9,19 @@ class Course < ApplicationRecord
   def to_s
     title
   end
+
   has_rich_text :description
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  # hex_secured
+  # friendly_id :generated_slug, use: :slugged
+  # def generated_slug
+  #   require 'securerandom'
+  #   @random_slug ||= persistented? ? fiendly_id : SecureRandom.hex(4)
+  # end
+
+  # def to_s
+  #   slug
+  # end
 end
