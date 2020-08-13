d04ecbca8cc5ff2006acab0e814ef831b19899f4
ext-nomad <i.dwanderer@gmail.com>
2020-08-13 17:12:06 +0300

    update styling of enrollments

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -11,9 +11,22 @@ module CoursesHelper
       else
         link_to 'Free', new_course_enrollment_path(course), class: 'btn btn-success'
       end
-
     else
       link_to 'Check price', course_path(course), class: 'btn btn-md btn-success'
+    end
+  end
+
+  def review_button(course)
+    user_course = course.enrollments.where(user: current_user)
+
+    if current_user
+      if user_course.any?
+        if user_course.pending_review.any?
+          link_to 'Add a review', edit_enrollment_path(user_course.first), class: 'btn btn-warning'
+        else
+          link_to 'Thanks for your review!', enrollment_path(user_course.first)
+        end
+      end
     end
   end
 end
