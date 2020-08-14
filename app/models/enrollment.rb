38f22131568bb30430b4f4b3c9c268fdce69faa7
ext-nomad <i.dwanderer@gmail.com>
2020-08-14 19:18:05 +0300

    navigation updated

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,6 +3,10 @@ class Enrollment < ApplicationRecord
   belongs_to :user
 
   validates :user, :course, presence: true
+
+  validates_presence_of :rating, if: :review?
+  validates_presence_of :review, if: :rating?
+
   validates_uniqueness_of :user_id, scope: :course_id
   validates_uniqueness_of :course_id, scope: :user_id
 
@@ -18,16 +22,6 @@ class Enrollment < ApplicationRecord
   end
 
   protected
-
-  # def cant_subscribe_to_own_course
-  #   if self.new_record?
-  #     if self.user_id.present?
-  #       if user_id == course.user_id
-  #         errors.add(:base, 'You can not subscribe to your own course')
-  #       end
-  #     end
-  #   end
-  # end
 
   def cant_subscribe_to_own_course
     if new_record? && user_id.present? && (user_id == course.user_id)
