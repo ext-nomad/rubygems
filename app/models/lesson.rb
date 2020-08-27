a895dd0be033417ea8e373f1a0d9ea3e32480588
ext-nomad <i.dwanderer@gmail.com>
2020-08-27 23:23:27 +0300

    style/ lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -21,6 +21,10 @@ class Lesson < ApplicationRecord
             content_type: %w[image/png image/jpg image/jpeg],
             size: { less_than: 500.kilobytes,
                     message: 'size should be under 500 kilobytes' }
+  # validates :video_thumbnail, presence: true, if: :video_present?
+  # def video_present?
+  #   video.present?
+  # end
 
   extend FriendlyId
   friendly_id :title, use: :slugged
