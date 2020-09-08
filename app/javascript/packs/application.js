faac40b6c2a4f8bfe0b7c4650d1169497565ae27
ext-nomad <i.dwanderer@gmail.com>
2020-09-08 18:06:02 +0300

    update/ adding new tags func

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -67,4 +67,13 @@ $(document).on("turbolinks:load", function () {
       sortField: "text",
     });
   }
+
+  $(".selectize-tags").selectize({
+    create: function (input, callback) {
+      $.post("/tags.json", { tag: { name: input } }).done(function (response) {
+        console.log(response);
+        callback({ value: response.id, text: response.name });
+      });
+    },
+  });
 });
