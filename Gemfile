5bb09f14ac9910f337b73093fda56cd6d0bf8509
ext-nomad <i.dwanderer@gmail.com>
2020-09-29 00:24:23 +0300

    update/ yarn/bundle

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.1'
 
+gem 'bootsnap', '>= 1.4.2', require: false
 gem 'jbuilder', '~> 2.7'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
@@ -13,8 +14,6 @@ gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 gem 'sass-rails', '>= 6'
 gem 'turbolinks', '~> 5'
 gem 'webpacker', '~> 5.x'
-
-gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,217 +10,216 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_162830) do
-
+ActiveRecord::Schema.define(version: 20_200_912_162_830) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  create_table 'action_text_rich_texts', force: :cascade do |t|
+    t.string 'name', null: false
+    t.text 'body'
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[record_type record_id name], name: 'index_action_text_rich_texts_uniqueness', unique: true
   end
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.bigint 'byte_size', null: false
+    t.string 'checksum', null: false
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "activities", force: :cascade do |t|
-    t.string "trackable_type"
-    t.bigint "trackable_id"
-    t.string "owner_type"
-    t.bigint "owner_id"
-    t.string "key"
-    t.text "parameters"
-    t.string "recipient_type"
-    t.bigint "recipient_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
-    t.index ["owner_type", "owner_id"], name: "index_activities_on_owner_type_and_owner_id"
-    t.index ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
-    t.index ["recipient_type", "recipient_id"], name: "index_activities_on_recipient_type_and_recipient_id"
-    t.index ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
-    t.index ["trackable_type", "trackable_id"], name: "index_activities_on_trackable_type_and_trackable_id"
+  create_table 'activities', force: :cascade do |t|
+    t.string 'trackable_type'
+    t.bigint 'trackable_id'
+    t.string 'owner_type'
+    t.bigint 'owner_id'
+    t.string 'key'
+    t.text 'parameters'
+    t.string 'recipient_type'
+    t.bigint 'recipient_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[owner_id owner_type], name: 'index_activities_on_owner_id_and_owner_type'
+    t.index %w[owner_type owner_id], name: 'index_activities_on_owner_type_and_owner_id'
+    t.index %w[recipient_id recipient_type], name: 'index_activities_on_recipient_id_and_recipient_type'
+    t.index %w[recipient_type recipient_id], name: 'index_activities_on_recipient_type_and_recipient_id'
+    t.index %w[trackable_id trackable_type], name: 'index_activities_on_trackable_id_and_trackable_type'
+    t.index %w[trackable_type trackable_id], name: 'index_activities_on_trackable_type_and_trackable_id'
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "lesson_id", null: false
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["lesson_id"], name: "index_comments_on_lesson_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table 'comments', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'lesson_id', null: false
+    t.text 'content'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['lesson_id'], name: 'index_comments_on_lesson_id'
+    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
-  create_table "course_tags", force: :cascade do |t|
-    t.bigint "course_id", null: false
-    t.bigint "tag_id", null: false
-    t.index ["course_id"], name: "index_course_tags_on_course_id"
-    t.index ["tag_id"], name: "index_course_tags_on_tag_id"
+  create_table 'course_tags', force: :cascade do |t|
+    t.bigint 'course_id', null: false
+    t.bigint 'tag_id', null: false
+    t.index ['course_id'], name: 'index_course_tags_on_course_id'
+    t.index ['tag_id'], name: 'index_course_tags_on_tag_id'
   end
 
-  create_table "courses", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.string "slug"
-    t.text "short_description"
-    t.string "language", default: "English", null: false
-    t.string "level", default: "Beginner", null: false
-    t.integer "price", default: 0, null: false
-    t.float "average_rating", default: 0.0, null: false
-    t.integer "enrollments_count", default: 0, null: false
-    t.integer "lessons_count", default: 0, null: false
-    t.boolean "published", default: false
-    t.boolean "approved", default: false
-    t.integer "income", default: 0, null: false
-    t.index ["slug"], name: "index_courses_on_slug", unique: true
-    t.index ["user_id"], name: "index_courses_on_user_id"
+  create_table 'courses', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.bigint 'user_id', null: false
+    t.string 'slug'
+    t.text 'short_description'
+    t.string 'language', default: 'English', null: false
+    t.string 'level', default: 'Beginner', null: false
+    t.integer 'price', default: 0, null: false
+    t.float 'average_rating', default: 0.0, null: false
+    t.integer 'enrollments_count', default: 0, null: false
+    t.integer 'lessons_count', default: 0, null: false
+    t.boolean 'published', default: false
+    t.boolean 'approved', default: false
+    t.integer 'income', default: 0, null: false
+    t.index ['slug'], name: 'index_courses_on_slug', unique: true
+    t.index ['user_id'], name: 'index_courses_on_user_id'
   end
 
-  create_table "enrollments", force: :cascade do |t|
-    t.bigint "course_id", null: false
-    t.bigint "user_id", null: false
-    t.integer "rating"
-    t.text "review"
-    t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "slug"
-    t.index ["course_id"], name: "index_enrollments_on_course_id"
-    t.index ["slug"], name: "index_enrollments_on_slug", unique: true
-    t.index ["user_id"], name: "index_enrollments_on_user_id"
+  create_table 'enrollments', force: :cascade do |t|
+    t.bigint 'course_id', null: false
+    t.bigint 'user_id', null: false
+    t.integer 'rating'
+    t.text 'review'
+    t.integer 'price'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'slug'
+    t.index ['course_id'], name: 'index_enrollments_on_course_id'
+    t.index ['slug'], name: 'index_enrollments_on_slug', unique: true
+    t.index ['user_id'], name: 'index_enrollments_on_user_id'
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  create_table 'friendly_id_slugs', force: :cascade do |t|
+    t.string 'slug', null: false
+    t.integer 'sluggable_id', null: false
+    t.string 'sluggable_type', limit: 50
+    t.string 'scope'
+    t.datetime 'created_at'
+    t.index %w[slug sluggable_type scope], name: 'index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope', unique: true
+    t.index %w[slug sluggable_type], name: 'index_friendly_id_slugs_on_slug_and_sluggable_type'
+    t.index %w[sluggable_type sluggable_id], name: 'index_friendly_id_slugs_on_sluggable_type_and_sluggable_id'
   end
 
-  create_table "lessons", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.bigint "course_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "slug"
-    t.integer "row_order"
-    t.integer "comments_count", default: 0, null: false
-    t.integer "user_lessons_count", default: 0, null: false
-    t.index ["course_id"], name: "index_lessons_on_course_id"
-    t.index ["slug"], name: "index_lessons_on_slug", unique: true
+  create_table 'lessons', force: :cascade do |t|
+    t.string 'title'
+    t.text 'content'
+    t.bigint 'course_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'slug'
+    t.integer 'row_order'
+    t.integer 'comments_count', default: 0, null: false
+    t.integer 'user_lessons_count', default: 0, null: false
+    t.index ['course_id'], name: 'index_lessons_on_course_id'
+    t.index ['slug'], name: 'index_lessons_on_slug', unique: true
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  create_table 'roles', force: :cascade do |t|
+    t.string 'name'
+    t.string 'resource_type'
+    t.bigint 'resource_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[name resource_type resource_id], name: 'index_roles_on_name_and_resource_type_and_resource_id'
+    t.index %w[resource_type resource_id], name: 'index_roles_on_resource_type_and_resource_id'
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.integer "course_tags_count", default: 0, null: false
+  create_table 'tags', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'course_tags_count', default: 0, null: false
   end
 
-  create_table "user_lessons", force: :cascade do |t|
-    t.bigint "lesson_id", null: false
-    t.bigint "user_id", null: false
-    t.integer "impressions", default: 0, null: false
-    t.index ["lesson_id"], name: "index_user_lessons_on_lesson_id"
-    t.index ["user_id"], name: "index_user_lessons_on_user_id"
+  create_table 'user_lessons', force: :cascade do |t|
+    t.bigint 'lesson_id', null: false
+    t.bigint 'user_id', null: false
+    t.integer 'impressions', default: 0, null: false
+    t.index ['lesson_id'], name: 'index_user_lessons_on_lesson_id'
+    t.index ['user_id'], name: 'index_user_lessons_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "slug"
-    t.integer "courses_count", default: 0, null: false
-    t.integer "enrollments_count", default: 0, null: false
-    t.integer "comments_count", default: 0, null: false
-    t.integer "user_lessons_count", default: 0, null: false
-    t.string "provider"
-    t.string "uid"
-    t.string "token"
-    t.integer "expires_at"
-    t.boolean "expires"
-    t.string "refresh_token"
-    t.string "image"
-    t.string "name"
-    t.integer "balance", default: 0, null: false
-    t.integer "course_income", default: 0, null: false
-    t.integer "enrollment_expenses", default: 0, null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["slug"], name: "index_users_on_slug", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.inet 'current_sign_in_ip'
+    t.inet 'last_sign_in_ip'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.string 'slug'
+    t.integer 'courses_count', default: 0, null: false
+    t.integer 'enrollments_count', default: 0, null: false
+    t.integer 'comments_count', default: 0, null: false
+    t.integer 'user_lessons_count', default: 0, null: false
+    t.string 'provider'
+    t.string 'uid'
+    t.string 'token'
+    t.integer 'expires_at'
+    t.boolean 'expires'
+    t.string 'refresh_token'
+    t.string 'image'
+    t.string 'name'
+    t.integer 'balance', default: 0, null: false
+    t.integer 'course_income', default: 0, null: false
+    t.integer 'enrollment_expenses', default: 0, null: false
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index ['slug'], name: 'index_users_on_slug', unique: true
   end
 
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "role_id"
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  create_table 'users_roles', id: false, force: :cascade do |t|
+    t.bigint 'user_id'
+    t.bigint 'role_id'
+    t.index ['role_id'], name: 'index_users_roles_on_role_id'
+    t.index %w[user_id role_id], name: 'index_users_roles_on_user_id_and_role_id'
+    t.index ['user_id'], name: 'index_users_roles_on_user_id'
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "lessons"
-  add_foreign_key "comments", "users"
-  add_foreign_key "course_tags", "courses"
-  add_foreign_key "course_tags", "tags"
-  add_foreign_key "courses", "users"
-  add_foreign_key "enrollments", "courses"
-  add_foreign_key "enrollments", "users"
-  add_foreign_key "lessons", "courses"
-  add_foreign_key "user_lessons", "lessons"
-  add_foreign_key "user_lessons", "users"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'comments', 'lessons'
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'course_tags', 'courses'
+  add_foreign_key 'course_tags', 'tags'
+  add_foreign_key 'courses', 'users'
+  add_foreign_key 'enrollments', 'courses'
+  add_foreign_key 'enrollments', 'users'
+  add_foreign_key 'lessons', 'courses'
+  add_foreign_key 'user_lessons', 'lessons'
+  add_foreign_key 'user_lessons', 'users'
 end
